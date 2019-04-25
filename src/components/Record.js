d5f4f057e86081899c32d4d686feefe38b1a5be7
Zeljko <zeljko.baranek@gmail.com>
2019-04-25 23:08:18 +0200

    Update description to include Chromebook

--- src/components/Record.js
+++ src/components/Record.js
@@ -118,7 +118,7 @@ class Record extends Component {
                         <Grid.Column></Grid.Column>
                         <Header as='h1'>Record computer screen video online</Header>
                         <p>Free, fast &amp; easy video capture - no additional software required<br />
-                            Works on PC with Windows, Mac and Linux OS in modern web browsers</p>
+                            Works on PC with Windows, Mac, Linux, Chromebook OS and web browser</p>
                     </div>
                     <Grid.Row>
                         {this.props.showBrowserUnsupported &&
