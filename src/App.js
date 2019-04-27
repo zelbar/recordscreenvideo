37b73934c508ac7fb6f7137427028e375827c9d4
Zeljko <zeljko.baranek@gmail.com>
2019-04-27 13:43:49 +0200

    Add new page components to app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router';
+import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Record from './components/Record';
+import RedirectToRecord from './components/RedirectToRecord';
 import Replay from './components/Replay';
-import Download from './components/Download';
+import Save from './components/Save';
 import Share from './components/Share';
+import HowItWorks from './components/HowItWorks';
+import Faq from './components/Faq';
+import Privacy from './components/Privacy';
 
 export default () => (
   <Layout>
     <Route exact path='/record' component={Record} />
-    <Redirect to='/record' />
+    <Route exact path='/' component={RedirectToRecord} />
     <Route exact path='/replay' component={Replay} />
-    <Route exact path='/download' component={Download} />
+    <Route exact path='/save' component={Save} />
     <Route exact path='/share' component={Share} />
+    <Route exact path='/how-it-works' component={HowItWorks} />
+    <Route exact path='/faq' component={Faq} />
+    <Route exact path='/privacy' component={Privacy} />
   </Layout>
 );
